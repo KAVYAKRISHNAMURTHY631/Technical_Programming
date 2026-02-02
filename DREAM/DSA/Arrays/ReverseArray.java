6d468a337cdd1870a4a59ff873ae99ec68bf72b2
KAVYA B K <kavyabk2002@gmail.com>
2026-02-02 19:57:33 +0530

    Added DSA array reverse program

---
+++ DREAM/DSA/Arrays/ReverseArray.java
@@ -0,0 +1,26 @@
+package DREAM.DSA.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        reverseUsingLoop(arr);
+    }
+
+    static void reverseUsingLoop(int[] arr) {
+        int i = 0, j = arr.length - 1;
+
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
